02042d3682025462e2b44bfb732238d31581e175
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-06-18 13:03:17 +0300

    create fetch newset books use case

---
+++ lib/feature/home/domain/use_cases/fetch_newset_books.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_clean_architecture_app/core/errors/failure.dart';
+import 'package:bookly_clean_architecture_app/feature/home/domain/entities/book_entity.dart';
+import 'package:bookly_clean_architecture_app/feature/home/domain/repos/home_repo.dart';
+import 'package:bookly_clean_architecture_app/feature/home/domain/use_cases/no_param_use_case.dart';
+
+import 'package:dartz/dartz.dart';
+
+class FetchNewsetBooksUseCase extends UseCase<List<BookEntity>> {
+  final HomeRepo homeRepo;
+
+  FetchNewsetBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchNewsetBooks();
+  }
+}
